30ce7747dde6342e0ea4acb5569db2f7a9f032e1
farirat <fourat@gmail.com>
2023-01-24 11:57:24 +0100

    Accounts organized

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,10 +1,6 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
-const OWNER_PRIVATE_KEY = "88fee7d4abf558ec3836285e3e51129abc620f243b149a17848b522c13ffc000";
-const LAMBDA01_PRIVATE_KEY = "88fce7d4abf558ec3836285e3e51129abc620f143b141a17842b528c12ffc001";
-const LAMBDA02_PRIVATE_KEY = "86fae7d4abf558ec3836285e3e51129abc620f443b143a17846b528c12ffc002";
-
 const config: HardhatUserConfig = {
   solidity: {
     version: "0.8.17",
@@ -18,7 +14,11 @@ const config: HardhatUserConfig = {
   networks: {
     goerli: {
       url: 'https://eth-goerli.public.blastapi.io',
-      accounts: [OWNER_PRIVATE_KEY, LAMBDA01_PRIVATE_KEY, LAMBDA02_PRIVATE_KEY]
+      accounts: [
+          "88fee7d4abf558ec3836285e3e51129abc620f243b149a17848b522c13ffc000",
+          "88fce7d4abf558ec3836285e3e51129abc620f143b141a17842b528c12ffc001",
+          "86fae7d4abf558ec3836285e3e51129abc620f443b143a17846b528c12ffc002"
+      ]
     }
   }
 };
